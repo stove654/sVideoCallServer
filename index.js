ea34065aa4fc7a5fadf7dfb483f21f3b63481b07
hoangvanloi <stove654@gmail.com>
2016-01-21 17:21:30 +0700

    update video call

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ app.use('/static', express.static('uploads'));
 require('./config/express')(app);
 require('./routes')(app);
 
+var ip = require('ip');
+var peerPort = 8100;
+var PeerServer = require('peer').PeerServer;
+var serverPeer = new PeerServer({port: peerPort, allow_discovery: true});
+
+serverPeer.on('connection', function (id) {
+    console.log('new connection with id ' + id);
+});
+
+serverPeer.on('disconnect', function (id) {
+    console.log('disconnect with id ' + id);
+});
+
+console.log('peer server running on ' + ip.address() + ':' + peerPort);
+
+
 app.get('/', function(request, response) {
     response.send('Hello World!');
 });
